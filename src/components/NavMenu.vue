800ed986fdf24ddc2d07202385aa60e8d72316da
Guzters <GaJR.B@outlook.com>
2023-02-06 20:56:15 -0600

    Modified colors & layout of navigation menu

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-const brand = ref('🏢Not Totlally Fake Company Directory')
+const brand = ref('🏢Not Totally Fake Company Directory🐱‍👤')
 </script>
 
 <template>
@@ -12,7 +12,7 @@ const brand = ref('🏢Not Totlally Fake Company Directory')
       <div class="menu">
         <a href="#" class="menu-item">Departments</a>
         <a href="#" class="menu-item">Settings</a>
-        <a href="#" class="menu-item">Logout</a>
+        <a href="#" class="menu-login">Logout</a>
       </div>
     </div>
   </nav>
@@ -20,9 +20,23 @@ const brand = ref('🏢Not Totlally Fake Company Directory')
 
 <style scoped lang="postcss">
 nav {
-  @apply h-20 bg-slate-900 text-slate-200;
+  @apply h-20 bg-stone-800 text-slate-100;
   .wrapper {
     @apply container mx-auto flex w-full items-center justify-between;
+    .brand {
+      &-title {
+        @apply text-2xl font-bold text-emerald-700;
+      }
+    }
+    .menu {
+      @apply flex gap-2;
+      &-item {
+        @apply rounded-md px-4 py-2 hover:bg-yellow-800 hover:text-slate-900;
+      }
+      &-login {
+        @apply rounded-md bg-red-500 px-4 py-2 text-slate-900 hover:bg-yellow-800;
+      }
+    }
   }
 }
 </style>

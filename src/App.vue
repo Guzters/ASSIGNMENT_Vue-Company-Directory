fbfbb85802cdd5c2a41348dcd6d6e23348c0f875
Guzters <GaJR.B@outlook.com>
2023-02-17 14:51:53 -0600

    Added a new page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import NavMenu from '@/components/NavMenu.vue'
 import MainPage from '@/components/MainPage.vue'
+
+import { ref } from 'vue'
+const showMainPage = ref(false)
 </script>
 
 <template>
   <NavMenu />
-  <MainPage />
+  <MainPage v-if="showMainPage" />
+  <div v-else>
+    <h1>This is another "Page"</h1>
+  </div>
 </template>

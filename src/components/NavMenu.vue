b6237fdef98d5fa23dc42f174b9c53ef864b913b
Guzters <GaJR.B@outlook.com>
2023-02-21 19:12:08 -0600

    Added FIREBASE authentication capabilities to the code and created a new file

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -17,7 +17,7 @@ const brand = ref(import.meta.env.VITE_APP_NAME)
         <i v-show="isAuthenticated" class="px-2 py-4">
           Welcome
           <strong
-            ><i>{{ user.name }}</i></strong
+            ><i>{{ user?.name }}</i></strong
           >
         </i>
         <div v-if="isAuthenticated">
